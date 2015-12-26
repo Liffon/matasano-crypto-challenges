9bec225263db6b3b99c14775ae80068823aed7e5
Rolf Lifvergren <rolli107@student.liu.se>
2015-12-26 21:16:39 +0100

    2-10 finished.

--- 2-10-cbc-mode.cpp
+++ 2-10-cbc-mode.cpp
@@ -1,21 +1,27 @@
 #include "matasano.h"
 #include "crypto.h"
 
-int main() {
-    buffer *input = read_until_eol();
-
+int main(int argc, char *argv[]) {
+    const char *input_filename = "2-10-input.txt";
+    const char *key = "YELLOW SUBMARINE";
     const char *iv = "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00";
+    if(argc >= 2) {
+        input_filename = argv[1];
+    }
+    if(argc >= 3) {
+        key = argv[2];
+    }
 
-    buffer *ciphertext = aes_128_cbc_encrypt(input, "YELLOW SUBMARINE", iv);
+    buffer *raw_ciphertext = read_file(input_filename);
+    buffer *ciphertext = base64_decode(raw_ciphertext);
+    free(raw_ciphertext);
 
-    printf("Ciphertext is:\n");
-    base64_print_buffer(ciphertext);
+    buffer *plaintext = aes_128_cbc_decrypt(ciphertext, key, iv);
 
-    buffer *plaintext = aes_128_cbc_decrypt(ciphertext, "YELLOW SUBMARINE", iv);
-    
-    printf("\nDecrypted text is:\n");
     literally_print_buffer(plaintext);
-    putchar('\n');
+
+    free(ciphertext);
+    free(plaintext);
 
     return 0;
 }
